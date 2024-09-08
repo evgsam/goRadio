200cb924aae5ea68ae6e140c04968fa8d6ab7845
evgsam <evgsamarin@gmail.com>
2024-09-08 22:08:01 +0300

    меню ввода данных

--- menu/inputMenu.go
+++ menu/inputMenu.go
@@ -2,105 +2,113 @@ package menu
 
 import (
 	"fmt"
-	component "goRadio/gocui-component"
+	"log"
+	"strings"
 
 	"github.com/jroimartin/gocui"
 )
 
-type signupForm struct {
-	*component.Form
+type Label struct {
+	name string
+	x, y int
+	w, h int
+	body string
 }
 
-func InputMenu() {
-	gui, err := gocui.NewGui(gocui.Output256)
+func NewLabel(name string, x, y int, body string) *Label {
+	lines := strings.Split(body, "\n")
 
+	w := 0
+	for _, l := range lines {
+		if len(l) > w {
+			w = len(l)
+		}
+	}
+	h := len(lines) + 1
+	w = w + 1
+
+	return &Label{name: name, x: x, y: y, w: w, h: h, body: body}
+}
+
+func (l *Label) Layout(g *gocui.Gui) error {
+	v, err := g.SetView(l.name, l.x, l.y, l.x+l.w, l.y+l.h)
 	if err != nil {
-		panic(err)
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+		v.Frame = false
+		fmt.Fprint(v, l.body)
 	}
-	defer gui.Close()
-
-	if err := gui.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quitForm); err != nil {
-		panic(err)
-	}
-
-	// new form
-	signup := &signupForm{
-		component.NewForm(gui, "Sign Up", 0, 0, 0, 0),
-	}
-
-	// add input field
-	signup.AddInputField("First Name", 11, 18).
-		AddValidate("required input", requireValidatorForm)
-	signup.AddInputField("Last Name", 11, 18).
-		AddValidate("required input", requireValidatorForm)
-
-	signup.AddInputField("Password", 11, 18).
-		AddValidate("required input", requireValidatorForm).
-		SetMask().
-		SetMaskKeybinding(gocui.KeyCtrlA)
-
-	// add checkbox
-	signup.AddCheckBox("Age 18+", 11)
-
-	// add select
-	signup.AddSelect("Language", 11, 10).AddOptions("Japanese", "English", "Chinese")
-
-	// add radios
-	signup.AddRadio("Country", 11).
-		SetMode(component.VerticalMode).
-		AddOptions("Japan", "America", "China")
-
-	// add button
-	signup.AddButton("Regist", signup.registF)
-	signup.AddButton("Cancel", quitForm)
-
-	signup.Draw()
-
-	if err := gui.MainLoop(); err != nil && err != gocui.ErrQuit {
-		panic(err)
-	}
-}
-
-func (s *signupForm) registF(g *gocui.Gui, v *gocui.View) error {
-	if !s.Validate() {
-		return nil
-	}
-
-	var text string
-
-	for label, ftext := range s.GetFieldTexts() {
-		text += fmt.Sprintf("%s: %s\n", label, ftext)
-	}
-
-	for label, state := range s.GetCheckBoxStates() {
-		text += fmt.Sprintf("%s: %t\n", label, state)
-	}
-
-	for label, opt := range s.GetSelectedOpts() {
-		text += fmt.Sprintf("%s: %s\n", label, opt)
-	}
-
-	text += fmt.Sprintf("radio: %s\n", s.GetSelectedRadios())
-
-	modal := component.NewModal(g, 0, 0, 30).SetText(text)
-	modal.AddButton("OK", gocui.KeyEnter, func(g *gocui.Gui, v *gocui.View) error {
-		modal.Close()
-		s.SetCurrentItem(s.GetCurrentItem())
-		return nil
-	})
-
-	modal.Draw()
-
 	return nil
 }
 
-func quitForm(g *gocui.Gui, v *gocui.View) error {
-	return gocui.ErrQuit
+type Input struct {
+	name      string
+	x, y      int
+	w         int
+	maxLength int
 }
 
-func requireValidatorForm(value string) bool {
-	if value == "" {
-		return false
+func NewInput(name string, x, y, w, maxLength int) *Input {
+	return &Input{name: name, x: x, y: y, w: w, maxLength: maxLength}
+}
+
+func (i *Input) Layout(g *gocui.Gui) error {
+	v, err := g.SetView(i.name, i.x, i.y, i.x+i.w, i.y+2)
+	if err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+		v.Editor = i
+		v.Editable = true
 	}
-	return true
+	return nil
+}
+
+func (i *Input) Edit(v *gocui.View, key gocui.Key, ch rune, mod gocui.Modifier) {
+	cx, _ := v.Cursor()
+	ox, _ := v.Origin()
+	limit := ox+cx+1 > i.maxLength
+	switch {
+	case ch != 0 && mod == 0 && !limit:
+		v.EditWrite(ch)
+	case key == gocui.KeySpace && !limit:
+		v.EditWrite(' ')
+	case key == gocui.KeyBackspace || key == gocui.KeyBackspace2:
+		v.EditDelete(true)
+	}
+}
+
+func SetFocus(name string) func(g *gocui.Gui) error {
+	return func(g *gocui.Gui) error {
+		_, err := g.SetCurrentView(name)
+		return err
+	}
+}
+
+func InputMenu() {
+	g, err := gocui.NewGui(gocui.OutputNormal)
+	if err != nil {
+		log.Panicln(err)
+	}
+	defer g.Close()
+
+	g.Cursor = true
+
+	label := NewLabel("label", 1, 1, "Name")
+	input := NewInput("input", 7, 1, 20, 10)
+	focus := gocui.ManagerFunc(SetFocus("input"))
+	g.SetManager(label, input, focus)
+
+	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quitImput); err != nil {
+		log.Panicln(err)
+	}
+
+	if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
+		log.Panicln(err)
+	}
+}
+
+func quitImput(g *gocui.Gui, v *gocui.View) error {
+	return gocui.ErrQuit
 }
