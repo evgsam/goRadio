73bf8a0e514e05c95f187a485a3ee4db3364913a
evgsam <evgsamarin@gmail.com>
2024-09-18 16:28:01 +0300

    рисую меню ввода

--- menu/inputMenu.go
+++ menu/inputMenu.go
@@ -2,106 +2,105 @@ package menu
 
 import (
 	"fmt"
+	"log"
+	"strings"
 
 	"github.com/jroimartin/gocui"
-
-	component "goRadio/gocui-component"
-	//component "github.com/skanehira/gocui-component"
 )
 
-type signupF struct {
-	*component.Form
+type Label struct {
+	name string
+	x, y int
+	w, h int
+	body string
 }
 
-func inputMenu() {
+func NewLabel(name string, x, y int, body string) *Label {
+	lines := strings.Split(body, "\n")
 
-	gui, err := gocui.NewGui(gocui.Output256)
+	w := 0
+	for _, l := range lines {
+		if len(l) > w {
+			w = len(l)
+		}
+	}
+	h := len(lines) + 2
+	w = w + 1
 
+	return &Label{name: name, x: x, y: y, w: w, h: h, body: body}
+}
+
+func (l *Label) Layout(g *gocui.Gui) error {
+	v, err := g.SetView(l.name, l.x, l.y, l.x+l.w, l.y+l.h)
 	if err != nil {
-		panic(err)
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+		v.Frame = false
+		fmt.Fprint(v, l.body)
 	}
-	defer gui.Close()
-
-	if err := gui.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
-		panic(err)
-	}
-
-	// new form
-	signup := &signupF{
-		component.NewForm(gui, "Sign Up", 0, 0, 0, 0),
-	}
-
-	// add input field
-	signup.AddInputField("First Name", 11, 18).
-		AddValidate("required input", requireValidatorF)
-	signup.AddInputField("Last Name", 11, 18).
-		AddValidate("required input", requireValidatorF)
-
-	signup.AddInputField("Password", 11, 18).
-		AddValidate("required input", requireValidatorF).
-		SetMask().
-		SetMaskKeybinding(gocui.KeyCtrlA)
-
-	// add checkbox
-	signup.AddCheckBox("Age 18+", 11)
-
-	// add select
-	signup.AddSelect("Language", 11, 10).AddOptions("Japanese", "English", "Chinese")
-
-	// add radios
-	signup.AddRadio("Country", 11).
-		SetMode(component.VerticalMode).
-		AddOptions("Japan", "America", "China")
-
-	// add button
-	signup.AddButton("Regist", signup.registF)
-	signup.AddButton("Cancel", quitF)
-
-	signup.Draw()
-
-	if err := gui.MainLoop(); err != nil && err != gocui.ErrQuit {
-		panic(err)
-	}
-}
-
-func (s *signupF) registF(g *gocui.Gui, v *gocui.View) error {
-	if !s.Validate() {
-		return nil
-	}
-
-	var text string
-
-	for label, ftext := range s.GetFieldTexts() {
-		text += fmt.Sprintf("%s: %s\n", label, ftext)
-	}
-
-	for label, state := range s.GetCheckBoxStates() {
-		text += fmt.Sprintf("%s: %t\n", label, state)
-	}
-
-	for label, opt := range s.GetSelectedOpts() {
-		text += fmt.Sprintf("%s: %s\n", label, opt)
-	}
-
-	text += fmt.Sprintf("radio: %s\n", s.GetSelectedRadios())
-
-	modal := component.NewModal(g, 0, 0, 30).SetText(text)
-	modal.AddButton("OK", gocui.KeyEnter, func(g *gocui.Gui, v *gocui.View) error {
-		modal.Close()
-		s.SetCurrentItem(s.GetCurrentItem())
-		return nil
-	})
-	modal.Draw()
 	return nil
 }
 
-func quitF(g *gocui.Gui, v *gocui.View) error {
-	return gocui.ErrQuit
+type Input struct {
+	name      string
+	x, y      int
+	w         int
+	maxLength int
 }
 
-func requireValidatorF(value string) bool {
-	if value == "" {
-		return false
+func NewInput(name string, x, y, w, maxLength int) *Input {
+	return &Input{name: name, x: x, y: y, w: w, maxLength: maxLength}
+}
+
+func (i *Input) Layout(g *gocui.Gui) error {
+	v, err := g.SetView(i.name, i.x, i.y, i.x+i.w, i.y+2)
+	if err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+		v.Editor = i
+		v.Editable = true
 	}
-	return true
+	return nil
+}
+
+func (i *Input) Edit(v *gocui.View, key gocui.Key, ch rune, mod gocui.Modifier) {
+	cx, _ := v.Cursor()
+	ox, _ := v.Origin()
+	limit := ox+cx+1 > i.maxLength
+	switch {
+	case ch != 0 && mod == 0 && !limit:
+		v.EditWrite(ch)
+	case key == gocui.KeySpace && !limit:
+		v.EditWrite(' ')
+	case key == gocui.KeyBackspace || key == gocui.KeyBackspace2:
+		v.EditDelete(true)
+	}
+}
+
+func SetFocus(name string) func(g *gocui.Gui) error {
+	return func(g *gocui.Gui) error {
+		_, err := g.SetCurrentView(name)
+		return err
+	}
+}
+
+func inputMenu(g *gocui.Gui, guiCh chan *gocui.Gui) {
+	g.Cursor = true
+
+	labelFreque := NewLabel("freque", 1, 1, "freque")
+	labelMode := NewLabel("mode", 1, 4, "mode")
+	inputFreque := NewInput("inputFreque", 7, 1, 20, 10)
+	focus := gocui.ManagerFunc(SetFocus("inputFreque"))
+	g.SetManager(labelFreque, labelMode, inputFreque, focus)
+
+	if err := initKeybindings_(g, guiCh); err != nil {
+		log.Panicln(err)
+	}
+
+	if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
+		log.Panicln(err)
+	}
+
 }
--- menu/mainMenu.go
+++ menu/mainMenu.go
@@ -59,42 +59,41 @@ func viewsValueUpdate(g *gocui.Gui, name, value string) error {
 	return nil
 }
 
-func dataToDisplay(g *gocui.Gui, ch chan map[byte]string, quitCh chan bool) {
+func dataToDisplay(ch chan map[byte]string, guiCh chan *gocui.Gui) {
+	var g *gocui.Gui
 	for {
-		select {
-		case <-quitCh:
-			return
-		default:
-			m := <-ch
-			g.Update(func(g *gocui.Gui) error {
-				for key, value := range m {
-					switch key {
-					case byte(status):
-						_ = viewsValueUpdate(g, viewsNames[byte(status)], value)
-					case byte(mode):
-						_ = viewsValueUpdate(g, viewsNames[byte(mode)], value)
-					case byte(att):
-						_ = viewsValueUpdate(g, viewsNames[byte(att)], value)
-					case byte(preamp):
-						_ = viewsValueUpdate(g, viewsNames[byte(preamp)], value)
-					case byte(freqRead):
-						_ = viewsValueUpdate(g, viewsNames[byte(freqRead)], value)
-					case byte(af):
-						_ = viewsValueUpdate(g, viewsNames[byte(af)], value)
-					case byte(rf):
-						_ = viewsValueUpdate(g, viewsNames[byte(rf)], value)
-					case byte(sql):
-						_ = viewsValueUpdate(g, viewsNames[byte(sql)], value)
-					}
-				}
-				return nil
-			})
+		if !dataUpdateFlag {
+			g = <-guiCh
+			dataUpdateFlag = true
 		}
-
+		m := <-ch
+		g.Update(func(g *gocui.Gui) error {
+			for key, value := range m {
+				switch key {
+				case byte(status):
+					_ = viewsValueUpdate(g, viewsNames[byte(status)], value)
+				case byte(mode):
+					_ = viewsValueUpdate(g, viewsNames[byte(mode)], value)
+				case byte(att):
+					_ = viewsValueUpdate(g, viewsNames[byte(att)], value)
+				case byte(preamp):
+					_ = viewsValueUpdate(g, viewsNames[byte(preamp)], value)
+				case byte(freqRead):
+					_ = viewsValueUpdate(g, viewsNames[byte(freqRead)], value)
+				case byte(af):
+					_ = viewsValueUpdate(g, viewsNames[byte(af)], value)
+				case byte(rf):
+					_ = viewsValueUpdate(g, viewsNames[byte(rf)], value)
+				case byte(sql):
+					_ = viewsValueUpdate(g, viewsNames[byte(sql)], value)
+				}
+			}
+			return nil
+		})
 	}
 }
 
-func delNewView_(g *gocui.Gui, ch chan map[byte]string, quitCh chan bool) error {
+func delNewView_(g *gocui.Gui, guiCh chan *gocui.Gui) error {
 	newGui = false
 	g.Close()
 
@@ -106,10 +105,12 @@ func delNewView_(g *gocui.Gui, ch chan map[byte]string, quitCh chan bool) error 
 
 	g.SetManagerFunc(layoutMainMenu)
 
-	if err := initKeybindings_(g, ch, quitCh); err != nil {
+	if err := initKeybindings_(g, guiCh); err != nil {
 		log.Panicln(err)
 	}
-	go dataToDisplay(g, ch, quitCh)
+
+	dataUpdateFlag = false
+	guiCh <- g
 
 	if err := g.MainLoop(); err != nil && !errors.Is(err, gocui.ErrQuit) {
 		log.Panicln(err)
@@ -117,36 +118,42 @@ func delNewView_(g *gocui.Gui, ch chan map[byte]string, quitCh chan bool) error 
 	return nil
 }
 
-func newView_(g *gocui.Gui, ch chan map[byte]string, quitCh chan bool) error {
+func newView_(g *gocui.Gui, guiCh chan *gocui.Gui) error {
 	if !newGui {
+		newGui = true
+		dataUpdateFlag = false
 		g.Close()
-		quitCh <- true
-		//dataUpdateFlag = false
-		//inputMenu()
-		g, err = gocui.NewGui(gocui.OutputNormal)
+		g, err := gocui.NewGui(gocui.OutputNormal)
 		if err != nil {
 			log.Panicln(err)
 		}
 		defer g.Close()
+		inputMenu(g, guiCh)
+		//widgets()
+		/*	g, err = gocui.NewGui(gocui.OutputNormal)
+			if err != nil {
+				log.Panicln(err)
+			}
+			defer g.Close()
 
-		g.SetManagerFunc(layoutNewMenu)
+			g.SetManagerFunc(layoutNewMenu)
 
-		if err := initKeybindings_(g, ch, quitCh); err != nil {
-			log.Panicln(err)
-		}
+			if err := initKeybindings_(g, guiCh); err != nil {
+				log.Panicln(err)
+			}
 
-		newGui = true
-		if err := g.MainLoop(); err != nil && !errors.Is(err, gocui.ErrQuit) {
-			log.Panicln(err)
-		}
-
+			newGui = true
+			if err := g.MainLoop(); err != nil && !errors.Is(err, gocui.ErrQuit) {
+				log.Panicln(err)
+			}
+		*/
 	}
 
 	return nil
 }
 
 func Menu(ch chan map[byte]string) {
-	quitCh := make(chan bool)
+	guiCn := make(chan *gocui.Gui)
 	viewArray = []viewsStruct{
 		{cmd: mainViews, name: "IC-78Information", x0: 0, y0: 0, x1: 50, y1: 7},
 		{cmd: status, name: "status", x0: 1, y0: 1, x1: 16, y1: 3},
@@ -169,10 +176,11 @@ func Menu(ch chan map[byte]string) {
 
 	g.SetManagerFunc(layoutMainMenu)
 
-	if err := initKeybindings_(g, ch, quitCh); err != nil {
+	if err := initKeybindings_(g, guiCn); err != nil {
 		log.Panicln(err)
 	}
-	go dataToDisplay(g, ch, quitCh)
+	go dataToDisplay(ch, guiCn)
+	guiCn <- g
 	if err := g.MainLoop(); err != nil && !errors.Is(err, gocui.ErrQuit) {
 		log.Panicln(err)
 	}
@@ -201,16 +209,16 @@ func layoutNewMenu(g *gocui.Gui) error {
 	return nil
 }
 
-func initKeybindings_(g *gocui.Gui, ch chan map[byte]string, quitCh chan bool) error {
-	if err := g.SetKeybinding("", 'n', gocui.ModNone,
+func initKeybindings_(g *gocui.Gui, guiCh chan *gocui.Gui) error {
+	if err := g.SetKeybinding("", gocui.KeyCtrlN, gocui.ModNone,
 		func(g *gocui.Gui, v *gocui.View) error {
-			return newView_(g, ch, quitCh)
+			return newView_(g, guiCh)
 		}); err != nil {
 		return err
 	}
-	if err := g.SetKeybinding("", 'b', gocui.ModNone,
+	if err := g.SetKeybinding("", gocui.KeyCtrlB, gocui.ModNone,
 		func(g *gocui.Gui, v *gocui.View) error {
-			return delNewView_(g, ch, quitCh)
+			return delNewView_(g, guiCh)
 		}); err != nil {
 		return err
 	}
