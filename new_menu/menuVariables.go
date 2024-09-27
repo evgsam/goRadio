87ddcaa8f5447fbe09b2828e415c183af927f58e
evgsam <evgsamarin@gmail.com>
2024-09-27 16:57:21 +0300

    вывел отдельный файл, в котором будут общие переменные меню

---
+++ new_menu/menuVariables.go
@@ -0,0 +1,31 @@
+package newmenu
+
+type commandName int
+
+const (
+	freqRead commandName = iota
+	taddr
+	mode
+	att
+	af
+	rf
+	sql
+	preamp
+	status
+	mainViews
+)
+
+var (
+	viewsNames      = make(map[byte]string)
+	infoViewArray   = make([]viewsStruct, 0)
+	hotkeyViewArray = make([]viewsStruct, 0)
+	inputViewArray  = make([]viewsStruct, 0)
+)
+
+type viewsStruct struct {
+	cmd            commandName
+	name           string
+	x0, y0, x1, y1 int
+	value          string
+	bottomFlag     bool
+}
